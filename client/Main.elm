2bb68462f841fd0373df45ed3a3bd4ef90e48c0a
Jeffrey Örnerstedt <jeffrey.ornerstedt@m.co>
2021-08-25 15:51:05 +0200

    add edit, complete and delete functionality

--- client/Main.elm
+++ client/Main.elm
@@ -2,13 +2,12 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, button, div, h2, input, p, text)
-import Html.Attributes exposing (placeholder, style)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (placeholder, style, value)
+import Html.Events exposing (onClick, onInput)
 
 
 type alias Model =
     { tasks : List Task
-    , currentId : Int
     , newTaskName : Maybe String
     }
 
@@ -17,25 +16,29 @@ type TaskStatus
     = Pending
     | Ongoing
     | Completed
+    | Editing
 
 
 type alias Task =
-    { id : Int
+    { id : SelectedTaskId
     , name : String
     , description : String
     , status : TaskStatus
     }
 
 
+type alias SelectedTaskId =
+    Int
+
+
 type Msg
-    = UpdateTask Int TaskStatus
-    | DeleteTask Int
+    = UpdateTask Task
+    | DeleteTask SelectedTaskId
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { tasks = defaultTasks
-      , currentId = List.length defaultTasks
       , newTaskName = Nothing
       }
     , Cmd.none
@@ -70,8 +73,20 @@ defaultTasks =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        UpdateTask selectedTaskID selectedTaskStatus ->
-            ( model, Cmd.none )
+        UpdateTask selectedTask ->
+            let
+                updatedTasks =
+                    List.map
+                        (\task ->
+                            if task.id == selectedTask.id then
+                                selectedTask
+
+                            else
+                                task
+                        )
+                        model.tasks
+            in
+            ( { model | tasks = updatedTasks }, Cmd.none )
 
         DeleteTask selectedTaskID ->
             ( { model
@@ -82,23 +97,81 @@ update msg model =
             )
 
 
-viewTaskCard : Task -> Html Msg
-viewTaskCard task =
-    div [ style "background-color" "lightgrey", style "margin-top" "1rem", style "width" "300px" ]
-        [ button [ onClick <| UpdateTask task.id Pending ] [ text "Edit" ]
-        , button [ onClick <| DeleteTask task.id ] [ text "X" ]
-        , div
-            [ style "padding" "1rem" ]
-            [ h2 [] [ text task.name ]
-            , p [] [ text task.description ]
-            ]
-        ]
-
-
 view : Model -> Html Msg
 view model =
     div []
         [ input [ placeholder "Enter new task" ] []
         , div [ style "display" "flex", style "flex-direction" "column", style "align-items" "flex-start" ]
-            (List.map (\task -> viewTaskCard task) model.tasks)
+            (List.map viewTaskCard model.tasks)
         ]
+
+
+viewTaskCard : Task -> Html Msg
+viewTaskCard task =
+    let
+        defaultWrapper children =
+            div [ style "background-color" "lightgrey", style "margin-top" "1rem", style "width" "300px" ] children
+
+        completedWrapper children =
+            div [ style "background-color" "green", style "margin-top" "1rem", style "width" "300px" ] children
+
+        editButton =
+            button [ onClick <| UpdateTask { task | status = Editing } ] [ text "Edit" ]
+
+        stopButton =
+            button [ onClick <| UpdateTask { task | status = Pending } ] [ text "Stop" ]
+
+        completeButton =
+            button [ onClick <| UpdateTask { task | status = Completed } ] [ text "Complete" ]
+
+        uncompleteButton =
+            button [ onClick <| UpdateTask { task | status = Pending } ] [ text "Uncomplete" ]
+
+        deleteButton =
+            button [ onClick <| DeleteTask task.id ] [ text "Delete" ]
+    in
+    case task.status of
+        Editing ->
+            defaultWrapper
+                [ stopButton
+                , completeButton
+                , deleteButton
+                , div
+                    [ style "padding" "1rem" ]
+                    [ h2 [] [ viewTaskNameInput task ]
+                    , p [] [ viewTaskDescriptionInput task ]
+                    ]
+                ]
+
+        Completed ->
+            completedWrapper
+                [ uncompleteButton
+                , deleteButton
+                , div
+                    [ style "padding" "1rem" ]
+                    [ h2 [] [ text task.name ]
+                    , p [] [ text task.description ]
+                    ]
+                ]
+
+        _ ->
+            defaultWrapper
+                [ editButton
+                , completeButton
+                , deleteButton
+                , div
+                    [ style "padding" "1rem" ]
+                    [ h2 [] [ text task.name ]
+                    , p [] [ text task.description ]
+                    ]
+                ]
+
+
+viewTaskNameInput : Task -> Html Msg
+viewTaskNameInput task =
+    input [ placeholder "Enter name", value task.name, onInput (\newValue -> UpdateTask { task | name = newValue }) ] []
+
+
+viewTaskDescriptionInput : Task -> Html Msg
+viewTaskDescriptionInput task =
+    input [ placeholder "Enter description", value task.description, onInput (\newValue -> UpdateTask { task | description = newValue }) ] []
